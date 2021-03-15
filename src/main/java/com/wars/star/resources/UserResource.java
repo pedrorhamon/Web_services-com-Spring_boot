9707c932e35abdd74998af765817ff25d917ba46
Pedro <pedrorhamon16@gmail.com>
2021-03-15 17:06:26 -0300

    User entity and resource

---
+++ src/main/java/com/wars/star/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.wars.star.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wars.star.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> fndAll(){
+		User u = new User(1L, "Pedro", "pedro@gmail.com", "988989898", "01234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
