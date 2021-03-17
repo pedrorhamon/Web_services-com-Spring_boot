6fabb3e5e28505b9130d3cae78535eced521dd0e
Pedro <pedrorhamon16@gmail.com>
2021-03-17 14:51:22 -0300

    Exception handling - findById

--- src/main/java/com/wars/star/services/UserService.java
+++ src/main/java/com/wars/star/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.wars.star.entities.User;
 import com.wars.star.repositories.UserRepository;
+import com.wars.star.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
