859e6a3248515224bf4f68e8c4f71e5aab787aef
Pedro <pedrorhamon16@gmail.com>
2021-03-16 21:37:58 -0300

    Category

--- src/main/java/com/wars/star/config/TestConfig.java
+++ src/main/java/com/wars/star/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.wars.star.entities.Category;
 import com.wars.star.entities.Order;
 import com.wars.star.entities.User;
 import com.wars.star.entities.enums.OrderStatus;
+import com.wars.star.repositories.CategoryRepository;
 import com.wars.star.repositories.OrderRepository;
 import com.wars.star.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
@@ -37,7 +48,4 @@ public class TestConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
-	
-	
-
 }
